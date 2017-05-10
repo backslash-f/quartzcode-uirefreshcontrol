adf739c8897145c10e017624a3a3e8ea4d446d79
Fernando Fernandes <mfernandes.fernando@gmail.com>
2017-05-10 10:24:56 -0300

    Updated code to use the new refreshControl property (ios 10).
     - https://useyourloaf.com/blog/refresh-control-changes-in-ios-10/
     - Also some code refactor after a SO user suggestion: http://stackoverflow.com/a/37162261/584548

--- QuartzCode-UIRefreshControl/Controller/TableViewController.swift
+++ QuartzCode-UIRefreshControl/Controller/TableViewController.swift
@@ -13,36 +13,27 @@ class TableViewController: UITableViewController {
     
     // MARK: - Properties
     
-    var scrollView: UIScrollView!
-    
-    // Reference to the original UIRefreshControl of this TableView.
-    @IBOutlet weak var originalRefreshControl: UIRefreshControl!
-    
     // Lazy reference to the custom UIRefreshControl made in QuartzCode.
     lazy var customUIRefreshControl: CustomUIRefreshControl = {
         
-        // The custom refresh control container size based on the original
-        // UIRefreshControl of this TableViewController.
-        let refreshControlContainerWidth: CGFloat = self.originalRefreshControl.bounds.size.width
-        let refreshControlContainerHeight: CGFloat = self.originalRefreshControl.bounds.size.height
+        // The custom refresh control container size based on the TableView's
+        // UIRefreshControl.
+        let containerWidth: CGFloat = self.refreshControl?.bounds.size.width ?? 40
+        let containerHeight: CGFloat = self.refreshControl?.bounds.size.height ?? 40
         
         // The size of the icon that goes inside the container (that blue cloud).
-        let customRefreshControlIconWidth: CGFloat = 40.0
-        let customRefreshControlIconHeight: CGFloat = 40.0
+        let iconWidth: CGFloat = 40.0, iconHeight: CGFloat = 40.0
         
-        // Position of the blue cloud inside its container.
-        let customRefreshControlIconXPosition: CGFloat = (refreshControlContainerWidth / 2.0) - (customRefreshControlIconWidth / 2.0)
-        let customRefreshControlIconYPosition: CGFloat = refreshControlContainerHeight / 2.0 - 1 // -1, otherwise it cuts the cloud's bottom a little bit :(
+        // Position of the blue cloud inside its container (center X and Y).
+        let iconXPosition: CGFloat = (containerWidth / 2.0) - (iconWidth / 2.0)
+        let iconYPosition: CGFloat = containerHeight / 2.0
         
         // Return it.
-        return CustomUIRefreshControl(frame: CGRect(x: customRefreshControlIconXPosition,
-                                                    y: customRefreshControlIconYPosition,
-                                                    width: customRefreshControlIconWidth,
-                                                    height: customRefreshControlIconHeight))
+        return CustomUIRefreshControl(frame: CGRect(x: iconXPosition,
+                                                    y: iconYPosition,
+                                                    width: iconWidth,
+                                                    height: iconHeight))
     }()
-    
-    // Helper to control the animation logic.
-    var isAnimating: Bool = false
     
     // MARK: - Lifecycle
     
@@ -53,105 +44,104 @@ class TableViewController: UITableViewController {
     
     // MARK: - Actions
     
-    /// Called everytime the original refresh control's value changes.
+    /// Called everytime refresh control's value changes.
     ///
-    /// - parameter sender: The original UIRefreshControl of this TableView.
+    /// - parameter sender: The `UIRefreshControl` of this TableView.
     @IBAction func refresh(_ sender: UIRefreshControl) {
         
-        // In this "demo", the refresh will last 6.0 seconds.
-        let delayInSeconds: Int64 = 6
+        animate()
         
-        let popTime = DispatchTime.now() + Double(delayInSeconds * Int64(NSEC_PER_SEC)) / Double(NSEC_PER_SEC)
-        
-        DispatchQueue.main.asyncAfter(deadline: popTime) {
+        // In this "demo", the refresh will last 5.0 seconds.
+        DispatchQueue.main.asyncAfter(deadline: .now() + 5) {
             
             // Do something with the retrieve data...
             // TODO
             
             // ... Then end the refresh operation.
-            self.originalRefreshControl.endRefreshing()
-            
-            // Re-enable pull down to refresh.
-            self.scrollView.isScrollEnabled = true
+            self.refreshControl?.endRefreshing()
             
             // Stop animations.
             self.stopAnimations()
         }
     }
-    
-    // MARK: - Scroll View Delegate
-    
+}
+
+// MARK: - Scroll View Delegate
+
+extension TableViewController {
+
+    /// The only reason I'm ovewriting this function is to add a nice fade in /
+    /// fade out effect on the icon of the custom refresh control.
     override func scrollViewDidScroll(_ scrollView: UIScrollView) {
         
-        // Update our scroll view reference.
-        self.scrollView = scrollView
-        
-        // If a refresh is already in place, do not mess up with alpha values
-        if (!originalRefreshControl.isRefreshing) {
-            
-            // Distance the table has been pulled >= 0.
-            let pullDistance: CGFloat = max(0.0, -originalRefreshControl.frame.origin.y)
-            
-            // Calculate the pull ratio, between 0.0-1.0.
-            let pullRatio: CGFloat = min(max(pullDistance, 0.0), 100.0) / 100.0
-            
-            // Update the cloud icon alpha for a nice effect.
-            customUIRefreshControl.alpha = pullRatio
-            
-        } else if (!isAnimating) {
-            // Double check if an animation isn't already in place
-            animate()
+        guard refreshControl != nil else {
+            return
         }
+        
+        // If a refresh operation is already in place, don't change the alpha
+        // values of the custom refresh control.
+        guard !refreshControl!.isRefreshing else {
+            return
+        }
+        
+        // Distance the table has been pulled >= 0.
+        let pullDistance: CGFloat = max(0.0, refreshControl!.frame.origin.y.negated())
+        
+        // Calculate the pull ratio, between 0.0-1.0.
+        let pullRatio: CGFloat = min(max(pullDistance, 0.0), 100.0) / 100.0
+        
+        // Update the cloud icon alpha for a nice effect.
+        customUIRefreshControl.alpha = pullRatio
+    }
+}
+
+
+// MARK: - Private
+
+fileprivate extension TableViewController {
+    
+    func setupCustomRefreshControl() {
+        
+        // Hide the "native" UIRefreshControl elements...
+        refreshControl?.tintColor = .clear
+        
+        // ... and add our custom refresh control:
+        refreshControl?.addSubview(customUIRefreshControl)
     }
     
-    // MARK: - Helpers
-    
-    fileprivate func setupCustomRefreshControl() {
-        
-        // Hide the original UIRefreshControl...
-        originalRefreshControl.tintColor = UIColor.clear
-        
-        // ... and ddd the custom refresh control instead:
-        originalRefreshControl.addSubview(customUIRefreshControl)
-    }
-    
-    fileprivate func animate() {
-        isAnimating = true
-        
-        // Disable scrolling when refreshing for a better experience.
-        scrollView.isScrollEnabled = false
-        
-        // Just to make sure that the icon is fully visible on the screen.
-        customUIRefreshControl.alpha = 1.0
-        
-        // Three examples. Uncomment / comment to check all of them:
-        animateCloudUpAndDown()
-        //animateCloudStrokeWithGradientFill()
-        //animateCloudStrokeWithSolidFill()
-    }
-    
-    fileprivate func stopAnimations() {
+    func stopAnimations() {
         customUIRefreshControl.removeAllAnimations()
-        isAnimating = false
+    }
+    
+    // Three examples. Uncomment / comment to check all of them.
+    func animate() {
+        
+        // Example 01.
+        animateCloudUpAndDown()
+        
+        // Example 02.
+        //animateCloudStrokeWithGradientFill()
+        
+        // Example 03.
+        //animateCloudStrokeWithSolidFill()
     }
     
     // MARK: - Animation Examples
     
     /// Animates the cloud up and down.
-    fileprivate func animateCloudUpAndDown() {
+    func animateCloudUpAndDown() {
         customUIRefreshControl.addRefreshUpDownAnimation()
     }
     
     /// "Draws" the cloud by make its stroke line gradually visible, then shows
     /// a solid blueish background and then fades everything out.
-    fileprivate func animateCloudStrokeWithGradientFill() {
+    func animateCloudStrokeWithGradientFill() {
         customUIRefreshControl.addRefreshGradientAnimation()
     }
     
     /// "Draws" the cloud by make its stroke line gradually visible, then shows
     /// a gradient blueish background and then fades everything out.
-    fileprivate func animateCloudStrokeWithSolidFill() {
+    func animateCloudStrokeWithSolidFill() {
         customUIRefreshControl.addRefreshSolidAnimation()
     }
 }
-
